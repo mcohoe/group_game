24e20770921c31352fdfb49661257515e64b42b1
David Cohoe <sudgylacmoe@gmail.com>
2016-08-30 14:01:53 -0700

    Added base entity class

---
+++ src/Game/entity.cpp
@@ -0,0 +1,26 @@
+// This is the implementation file for entities.
+
+#include "entity.h"
+
+Entity::Entity(int x, int y)
+{
+    tile_x = x;
+    tile_y = y;
+}
+
+void Entity::update()
+{
+    x_vel += x_accel;
+    y_vel += y_accel;
+    if (x_vel < -MAX_X_VEL) x_vel = -MAX_X_VEL;
+    else if (x_vel > MAX_X_VEL) x_vel = MAX_X_VEL;
+    if (y_vel < -MAX_Y_VEL) y_vel = -MAX_Y_VEL;
+    else if (y_vel > MAX_Y_VEL) y_vel = MAX_Y_VEL;
+
+    subpixel_x += x_vel;
+    subpixel_y += y_vel;
+    pixel_x = subpixel_x % PIXEL_WIDTH;
+    pixel_y = subpixel_y % PIXEL_HEIGHT;
+    tile_x = pixel_x % TILE_WIDTH;
+    tile_y = pixel_y % TILE_HEIGHT;
+}
---
+++ src/Game/entity.h
@@ -0,0 +1,33 @@
+// This is the header file for entities.
+
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include "tile.h"
+
+class Entity {
+    public:
+        Entity(int x = 0, int y = 0);
+        void update();
+
+        static const int PIXEL_WIDTH = 16;
+        static const int PIXEL_HEIGHT = 16;
+        static const int TILE_WIDTH = Tile::TILE_WIDTH;
+        static const int TILE_HEIGHT = Tile::TILE_HEIGHT;
+        static const int MAX_X_VEL = 40;
+        static const int MAX_Y_VEL = 100;
+
+    private:
+        int subpixel_x = 0;
+        int subpixel_y = 0;
+        int pixel_x = 0;
+        int pixel_y = 0;
+        int tile_x = 0;
+        int tile_y = 0;
+        int x_vel = 0;
+        int y_vel = 0;
+        int x_accel = 0;
+        int y_accel = 0;
+};
+
+#endif
