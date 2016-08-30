5a6166b44d2c07e4546f616ba4416309999e7c22
Michael Cohoe <michaelccohoe@gmail.com>
2016-08-30 13:46:30 -0700

    Added sky tile and changed default tile to empty

--- src/Game/tile.h
+++ src/Game/tile.h
@@ -9,13 +9,13 @@
 class Tile {
 
    public:
-        enum Id{Air, Stone};                                //enum that holds every possible tile type
-        Tile(Id to_set = Air, Graphics * graphics_ = NULL); //contrstuctor that takes in a tile id and the graphics. Builds an air tile if none are passed in.
-        void change_tile(Id to_set);                        //function that changes the tile type
-        void display_at(int x, int y);                      //function that sets the tile's sprite's x and y 
+        enum Id{Empty, Sky, Stone};                           //enum that holds every possible tile type
+        Tile(Id to_set = Empty, Graphics * graphics_ = NULL); //contrstuctor that takes in a tile id and the graphics. Builds an empty tile if none are passed in.
+        void change_tile(Id to_set);                          //function that changes the tile type
+        void display_at(int x, int y);                        //function that sets the tile's sprite's x and y 
 
-        static const int TILE_WIDTH = 32;                   //width of a tile
-        static const int TILE_HEIGHT = 32;                  //height of a tile
+        static const int TILE_WIDTH = 32;                     //width of a tile
+        static const int TILE_HEIGHT = 32;                    //height of a tile
 
    private:
         Sprite sprite;                                      //sprite of the tile
