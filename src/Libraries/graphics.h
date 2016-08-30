182b22062e283c24eeca6410b722b631746fb659
Michael Cohoe <michaelccohoe@gmail.com>
2016-08-30 12:39:51 -0700

    Made const variables public

--- src/Libraries/graphics.h
+++ src/Libraries/graphics.h
@@ -18,18 +18,20 @@ class Graphics
         Graphics& operator=(const Graphics&) = delete;
         Graphics(const Graphics&) = delete;
 
-        void update(); // Redraws the screen and waits for the frame to finish
-        void add_sprite(Sprite* s) {sprites.push_back(s);} // Add a sprite to the sprite list
-        void remove_sprite(Sprite* s); // Remove a sprite from the sprite list
-        static bool get_initialized() {return initialized;}
-        SDL_Renderer* get_renderer() {return renderer;}
-    private:
         // Screen height and width
         static const int SCREEN_WIDTH = 640;
         static const int SCREEN_HEIGHT = 480;
         // Framerate
         static const int FRAMERATE = 60;
         static const int TICKS_PER_FRAME = 1000 / FRAMERATE;
+
+        void update(); // Redraws the screen and waits for the frame to finish
+        void add_sprite(Sprite* s) {sprites.push_back(s);} // Add a sprite to the sprite list
+        void remove_sprite(Sprite* s); // Remove a sprite from the sprite list
+        static bool get_initialized() {return initialized;}
+        SDL_Renderer* get_renderer() {return renderer;}
+
+    private:
         int current_frame_start_ticks = 0; // How many ticks it's taken to get to this frame
         // The window and renderer the game renders to
         SDL_Window* window = NULL;
