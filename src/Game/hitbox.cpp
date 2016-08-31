491c8d387e209ab9f6f94ea448e72b741d0e0326
Michael Cohoe <michaelccohoe@gmail.com>
2016-08-30 19:53:05 -0700

    Added function to hitbox that checks to see what side is being touched

--- src/Game/hitbox.cpp
+++ src/Game/hitbox.cpp
@@ -30,3 +30,36 @@ bool Hitbox::is_touching(const Hitbox & other){
     }
     else return false;
 }
+
+//Function that checks to see if a certain side is touched
+bool Hitbox::touching_side(const Hitbox & other, Direction to_check){
+    if (!is_touching(other))
+        return false;
+
+    if (to_check == Left){
+        if (x < other.x + other.width and x > other.x)
+            return true;
+    }
+
+    if (to_check == Right){
+        if (x + width < other.x + other.width and x + width > other.x)
+            return true;
+    }
+    if (to_check == Up){
+        if (y < other.y + other.height and y > other.y)
+            return true;
+    }
+    if (to_check == Down){
+        if (y + height < other.y + other.height and y + height > other.y)
+            return true;
+    }
+    return false;    
+
+}
+
+
+
+
+
+
+
